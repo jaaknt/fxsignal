14aabacb273ca5e4c0b848eb0732182968ddfcab
Jaak Niit <jaak.niit@gmail.com>
2019-05-02 20:04:48 +0300

    adding fleep message functionality

--- fxsignal/__init__.py
+++ fxsignal/__init__.py
@@ -1,4 +1,5 @@
 from .runner import btester
 from .feed import FxcmFeed, Feed
 from .feedconverter import FeedConverter
+from .message import Fleep
 # from .trend_keltner import BasicStrategy, BuyStrategy, SellStrategy
